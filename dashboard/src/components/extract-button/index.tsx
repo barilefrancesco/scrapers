c8b839fc5dbe95b1b924dafa581e870560853207
barilefrancesco <barile.ciccio98@gmail.com>
2022-12-17 00:33:04 +0100

    Fix modal GUI

--- dashboard/src/components/extract-button/index.tsx
+++ dashboard/src/components/extract-button/index.tsx
@@ -47,7 +47,8 @@ const ExtractButton = ({
   const onSubmit: SubmitHandler<FormInput> = async ({
     scrapers,
     phone,
-    contacts,
+    contacts_wa,
+    contacts_tl,
     telegram_api_id,
     telegram_api_hash,
     telegram_session_code,
@@ -72,7 +73,7 @@ const ExtractButton = ({
                         ? telegram_session_code
                         : TelegramIDs.FIRST_SESSION_CODE,
                       phone: phone,
-                      // contacts: contacts ? contacts : "",
+                      // contacts: contacts_tl ? contacts_tl : "",
                     }),
                   {
                     method: "GET",
@@ -87,8 +88,8 @@ const ExtractButton = ({
             }
           } else if (scraper === Scrapers.WHATSAPP) {
             let params = "";
-            if (contacts) {
-              params = `?contacts=${contacts}`;
+            if (contacts_wa) {
+              params = `?contacts=${contacts_wa}`;
             }
             const response = await (
               await fetch(LINKS[scraper] + params, {
@@ -142,49 +143,84 @@ const ExtractButton = ({
                 {/*body*/}
                 <form onSubmit={handleSubmit(onSubmit)}>
                   <div className="relative flex-auto p-3">
-                    <div className="flex flex-col p-4">
-                      <label>
-                        {t("modal-extract-choose-scrapers", {
-                          ns: ["labels"],
-                        })}{" "}
-                        <span className="text-red-600">*</span>
-                      </label>
-                      <Controller
-                        name="scrapers"
-                        control={control}
-                        rules={{ required: true }}
-                        render={({ field }) => (
-                          <Select
-                            isMulti
-                            components={animatedComponents}
-                            {...field}
-                            onChange={(value) =>
-                              field.onChange(value.map((v) => v.value))
-                            }
-                            options={options}
-                            value={options.filter((option) =>
-                              field.value?.find((v) => v === option.value)
-                            )}
-                          />
-                        )}
-                      />
+                    <div className="flex w-full justify-between">
+                      <div className="flex w-full flex-col p-4">
+                        <label>
+                          {t("modal-extract-choose-scrapers", {
+                            ns: ["labels"],
+                          })}{" "}
+                          <span className="text-red-600">*</span>
+                        </label>
+                        <Controller
+                          name="scrapers"
+                          control={control}
+                          rules={{ required: true }}
+                          render={({ field }) => (
+                            <Select
+                              isMulti
+                              components={animatedComponents}
+                              {...field}
+                              onChange={(value) => {
+                                field.onChange(value.map((v) => v.value));
+                              }}
+                              options={options}
+                              value={options.filter((option) =>
+                                field.value?.find((v) => v === option.value)
+                              )}
+                            />
+                          )}
+                        />
+                      </div>
+                      <div className="flex w-full flex-col p-4">
+                        <label>
+                          {t("modal-extract-phone-number", {
+                            ns: ["labels"],
+                          })}{" "}
+                          <span className="text-red-600">*</span>
+                        </label>
+                        <input
+                          className="inline-block rounded-lg px-3 py-1.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+                          type="text"
+                          placeholder="3333333333"
+                          {...register("phone", { required: true })}
+                        />
+                      </div>
                     </div>
-                    <div className="flex flex-col p-4">
-                      <label>
-                        {" "}
-                        {t("modal-extract-contacts-list", {
-                          ns: ["labels"],
-                        })}
-                      </label>
-                      <input
-                        className="inline-block rounded-lg px-3 py-1.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-                        type="text"
-                        placeholder="Mario,Sara,+393333333333"
-                        {...register("contacts")}
-                      />
+                    <div className="flex w-full justify-between">
+                      <div className="flex w-full flex-col p-4">
+                        <label>
+                          {" "}
+                          {t("modal-extract-contacts-list-wa", {
+                            ns: ["labels"],
+                          })}
+                        </label>
+                        <input
+                          className="inline-block rounded-lg px-3 py-1.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+                          type="text"
+                          placeholder="Mario,Sara,+393333333333"
+                          {...register("contacts_wa")}
+                        />
+                      </div>
+                      <div className="flex w-full flex-col p-4">
+                        <label>
+                          {" "}
+                          {t("modal-extract-contacts-list-tl", {
+                            ns: ["labels"],
+                          })}
+                        </label>
+                        <input
+                          className="inline-block rounded-lg px-3 py-1.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+                          type="text"
+                          placeholder="Mario,Sara,+393333333333"
+                          {...register("contacts_tl")}
+                        />
+                      </div>
                     </div>
-                    <div className="flex justify-between p-4">
-                      <div className="grid">
+                    <div
+                      id="telegramFields"
+                      className="flex w-full justify-between"
+                    >
+                      <div className="flex w-full flex-col p-4">
                         <label>
                           {" "}
                           {t("modal-extract-telegram-api-id", {
@@ -198,7 +234,7 @@ const ExtractButton = ({
                           {...register("telegram_api_id")}
                         />
                       </div>
-                      <div className="grid">
+                      <div className="flex w-full flex-col p-4">
                         <label>
                           {" "}
                           {t("modal-extract-telegram-session-code", {
@@ -212,7 +248,7 @@ const ExtractButton = ({
                           {...register("telegram_api_hash")}
                         />
                       </div>
-                      <div className="grid">
+                      <div className="flex w-full flex-col p-4">
                         <label>
                           {" "}
                           {t("modal-extract-telegram-session-code", {
@@ -226,20 +262,6 @@ const ExtractButton = ({
                           {...register("telegram_session_code")}
                         />
                       </div>
-                    </div>
-                    <div className="flex flex-col p-4">
-                      <label>
-                        {t("modal-extract-phone-number", {
-                          ns: ["labels"],
-                        })}{" "}
-                        <span className="text-red-600">*</span>
-                      </label>
-                      <input
-                        className="inline-block rounded-lg px-3 py-1.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-                        type="text"
-                        placeholder="3333333333"
-                        {...register("phone", { required: true })}
-                      />
                     </div>
                   </div>
                   <div className="flex items-center justify-end rounded-b border-t border-solid border-slate-200 p-3">
